de3305bac5efe36ea001af4c6aeef0d9817bdf9f
safelyafnan <80144801+safelyafnan@users.noreply.github.com>
2021-06-18 16:10:55 +0700

    Update Program Pintu Tol.py

--- Program Pintu Tol.py
+++ Program Pintu Tol.py
@@ -1,19 +1,8 @@
-import random
-daftar_tgl = ("1", "2", "3", "4", "5", "06", "7", "8", "9" "10", "11", "12", "13", "14", "15", "16", "17",
-              "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30")
-bulan = ("1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12")
-
-rand = random.randint(0, 12)
-rando = random.randint(0, 31)
-
-today = daftar_tgl[rando]
-month = bulan[rand]
-tanggal = today + "-" + month + "-2021"
-
 print('--------------------------------------------')
 print('Selamat datang di pintu masuk tol Jasawarga')
 print('--------------------------------------------')
-
+#Tanggal saat ini
+tanggal =input('silakan masukkan tanggal saat ini (format:DD-MM-YYYY): ')
 #Lokasi saat ini
 print('Lokasi anda saat ini adalah')
 print('A. Semarang')
@@ -444,27 +433,27 @@ elif lokasi == 'E':
 print('\n')
 
 #Tarif tambahan mendekati lebaran
-if tanggal == '6-5-2021':
+if tanggal == '6-05-2021':
     harga1 = harga1 + 5000
     print('========================================================================================================')
     print('Dikarenakan sudah memasuki H-7 lebaran maka ada tambahan tarif sebesar 7000 sehingga menjadi IDR', harga1)
     print('========================================================================================================')
-elif tanggal == '10-5-2021':
+elif tanggal == '10-05-2021':
     harga1 = harga1 + 7000
     print('========================================================================================================')
     print('Dikarenakan sudah memasuki H-3 lebaran maka ada tambahan tarif sebesar 7000 sehingga menjadi IDR', harga1)
     print('========================================================================================================')
-elif tanggal == '13-5-2021':
+elif tanggal == '13-05-2021':
     harga1 = harga1 + 10000
     print('===========================================================================================================')
     print('Dikarenakan sudah memasuki hari H lebaran maka ada tambahan tarif sebesar 7000 sehingga menjadi IDR', harga1)
     print('===========================================================================================================')
-elif tanggal == '16-5-2021':
+elif tanggal == '16-05-2021':
     print('========================================================================================================')
     harga1 = harga1 + 7000
     print('Dikarenakan sudah memasuki H+3 lebaran maka ada tambahan tarif sebesar 7000 sehingga menjadi IDR', harga1)
     print('========================================================================================================')
-elif tanggal == '20-5-2021':
+elif tanggal == '20-05-2021':
     harga1 = harga1 + 5000
     print('========================================================================================================')
     print('Dikarenakan sudah memasuki H+7 lebaran maka ada tambahan tarif sebesar 7000 sehingga menjadi IDR', harga1)
@@ -478,16 +467,16 @@ print('Semoga selamat sampai tujuan. Berikut adalah struk transaksi anda:')
 print('')
 print('Kota tujuan                              :', Tujuan)
 print('Jenis kendaraan anda adalah              :', jenis_kendaraan)
-if tanggal == '6-5-2021':
-    print('Tarif tambahan                       :IDR 5000')
-elif tanggal == '10-5-2021':
-    print('Tarif tambahan                       :IDR 7000')
-elif tanggal == '13-5-2021':
-    print('Tarif tambahan                       :IDR 10000')
-elif tanggal == '16-5-2021':
-    print('Tarif tambahan                       :IDR 7000')
-elif tanggal == '20-5-2021':
-    print('Tarif tambahan                       :IDR 5000')
+if tanggal == '6-05-2021':
+    print('Tarif tambahan                           : IDR 5000')
+elif tanggal == '10-05-2021':
+    print('Tarif tambahan                           : IDR 7000')
+elif tanggal == '13-05-2021':
+    print('Tarif tambahan                           : IDR 10000')
+elif tanggal == '16-05-2021':
+    print('Tarif tambahan                           : IDR 7000')
+elif tanggal == '20-05-2021':
+    print('Tarif tambahan                           : IDR 5000')
 print('Harga yang harus anda bayarkan adalah    : IDR', harga1)
 print('')
 print('WWW.Jasa-Warga.com\tCall Center:777\t', tanggal)
